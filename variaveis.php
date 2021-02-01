ba8af057bbb60f9614340349158d4c277f7e7b42
MatheusDuarte <matheusfabuloso@hotmail.com>
2021-01-31 23:38:47 -0300

    variaveis

---
+++ variaveis.php
@@ -0,0 +1,22 @@
+<?php 
+
+    /**
+     * Não iniciar com numeros
+     * Usar camelCase
+     * Não usar caracteres especiais
+     * Não usar espaço
+    */
+
+    $lastName = "Matheus";
+    $age = 23;
+
+    echo "Minha idade é ".$age; // Concatenação
+    unset($age);
+
+    echo $age;
+    //var_dump($age);
+
+
+
+
+?>
